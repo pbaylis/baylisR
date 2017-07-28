d1863bdf363e410cbfbc2c8156337ed5f0e1f58a
Patrick Baylis <pbaylis@gmail.com>
2017-07-28 09:02:20 -0700

    new idea: load a precomputed tz raster
    tz raster is from running fasterize on a MODIS raster + timezone
    shapefile

--- R/import_twitter_files.R
+++ R/import_twitter_files.R
@@ -1,22 +1,3 @@
-import_twitter_files <- function(inFiles, col_types=NULL, rast.file=NULL, keep_UTC=FALSE) {
-  #' This function imports many CSVs of twitter data and collects them into a single data.table.
-  #' @param inFiles Set of files to import. Presumably a list of .csv or .csv.gz.
-  #' @param col_types Passed through to read_csv. NULL imports all fields.
-  #' @param keep_UTC Flag to return UTC datetime.
-  #' @return Returns data.table.
-  #' import_twitter_files()
-  require(raster)
-  require(readr)
-  inFiles <- list("B:/twitter_data/usa_geo/scores/usa_geo_backup_2014-05-22_scores.csv")
-  col_types <- cols_only(tweet_datetime=col_datetime(format = ""),
-                      lng=col_double(), lat=col_double(),
-                      afinn="?", vaderPos="?", vaderNeg="?")
-  rast.file <- "B:/Dropbox/01_Work/01_Current/99_Common/geo/MODIS1k_rasterized.tif"
-  rast <- raster(rast.file)
-  return(rbindlist(lapply(inFiles, import_twitter_csv, col_types=col_types, rast=rast, keep_UTC=keep_UTC),
-                   use.names=TRUE))
-}
-
 import_twitter_file <- function(inFile, col_types=NULL, rast=NULL, keep_UTC=FALSE) {
   #' This function imports a single CSV of twitter data.
   #' @param inFile Filename to import (.csv or .csv.gz).
@@ -28,13 +9,13 @@ import_twitter_file <- function(inFile, col_types=NULL, rast=NULL, keep_UTC=FALS
   require(raster)
   require(readr)
   tic <- Sys.time()
-  
+
   inFile <- "B:/twitter_data/usa_geo/scores/usa_geo_backup_2014-05-22_scores.csv"
   col_types <- cols_only(tweet_datetime=col_datetime(format = ""),
                          lng=col_double(), lat=col_double(),
                          afinn="?", vaderPos="?", vaderNeg="?")
   keep_UTC <- TRUE
-  
+
   if (file.exists(inFile)) {
     print(paste0("Loading ", inFile))
 
@@ -44,12 +25,6 @@ import_twitter_file <- function(inFile, col_types=NULL, rast=NULL, keep_UTC=FALS
     if ("postedTime" %in% names(scores)) {setnames(scores, "postedTime", "datetimeUTC")}
     if ("tweet_datetime" %in% names(scores)) {setnames(scores, "tweet_datetime", "datetimeUTC")}
 
-    # Get MODIS raster data
-    scores[, cell.MODIS1k := cellFromXY(rast, scores[, list(lng, lat)])]
-
-    scores[, TZID:=test <- getValues(rast)[cell.MODIS1k]] 
-    scores[, `:=`(date=as.IDate(datetimeUTC, tz=TZID)), by=TZID] # Get local date (could get time but aggregating anyway)
-
     print(Sys.time() - tic)
     return(scores)
   }
@@ -57,5 +32,34 @@ import_twitter_file <- function(inFile, col_types=NULL, rast=NULL, keep_UTC=FALS
   return(NULL)
 }
 
-# another option - just have a fxn here for importing one twitter file. then have another utility fxn for 
-# adding data via a raster. be good to test if that's not too memory intensive on massive datasets.
+add_raster_data <- function(dt, rast, rast.levels=NULL) {
+  require(raster)
+  scores[, cell.rast := cellFromXY(rast, scores[, list(lng, lat)])]
+
+  scores[, rast.value:=getValues(rast)[cell.rast]]
+
+  # Replace numeric with levels
+  if (!is.null(rast.levels)) {
+    scores[, rast.value2:=rast.levels[rast.value]]
+    scores[, rast.value:=NULL]
+    setnames(scores, "rast.value2", "rast.value")
+  }
+
+}
+
+get_local_time <- function(dt) {
+  # dt with following cols (at minimum): lng, lat, tweet_datetime
+  # return same dt plus cell plus local date and time
+  require(fasterize)
+  require(sf)
+  require(raster)
+  MODIS.rast <- raster(MODIS.file) # Is there a reliable way to include data in packages?
+  tz.shp <-
+  tz.rast <- fasterize(MODIS.rast, tz.shp)
+  rast.fact <- levels(tz.rast)
+  add_raster_data(dt, tz.rast, rast.levels)
+
+
+  scores[, `:=`(date=as.IDate(datetimeUTC, tz=TZID)), by=TZID] # Get local date (could get time but aggregating anyway)
+
+}
