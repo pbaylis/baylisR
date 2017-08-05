46e0190f4e67e9434c7e371dd660aacb02e65199
Patrick Baylis <pbaylis@gmail.com>
2017-08-04 23:16:52 -0800

    actually document commands

--- roxygen_commands.R
+++ roxygen_commands.R
@@ -2,12 +2,11 @@ library(devtools)
 library(roxygen2)
 
 setwd("~/github/baylisR")
-setwd("B:/github/baylisR")
+#setwd("B:/github/baylisR")
 # create("baylisR")
 document()
 install(".")
 install_github("pbaylis/baylisR", auth_token="ebb64529e5a9127f17624c261dbd727b4ab7c9e6")
-
 
 # rasterize tz.shp
 library(raster)
